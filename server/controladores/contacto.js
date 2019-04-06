bb086de837bf3b7c4d96fe4ee0e667022447738b
3a8i3 <adavidruizdiaz@gmail.com>
2019-04-06 19:13:28 -0400

    GETS /contacto, /contacto/id, /contacto/bustar/nombre_o_telefono

--- server/controladores/contacto.js
+++ server/controladores/contacto.js
@@ -38,6 +38,98 @@ app.post('/contacto', (req, res) => {
 
 });
 
+/**
+ * <GETS>
+ */
+
+//GET all
+app.get('/contacto', (req, res) => {
+	
+	Contacto.find({ activo:true }, 'nombre apellido telefono email direccion')
+		.sort('nombre')      //orden
+		.exec( (err, contactos) => {
+			if ( err ) {
+				return res.status(400).json({
+					ok: false,
+					err
+				})
+			}
+
+			Contacto.count({}, (err, c) => {
+				res.json({
+					ok:true,
+					registros: c,
+					contactos
+				});
+			});
+
+
+		})
+
+});
+
+// GET con parametro id
+app.get('/contacto/:id', (req, res) => {
+    
+    let id = req.params.id;
+    
+    Contacto.find({ $and: [{_id:{$eq:id}, activo: { $eq: true } }] },
+        'nombre apellido telefono email direccion',
+        ( err, contacto ) => {
+    
+            if ( err ) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            
+            res.json({
+                ok: true,
+                contacto
+            })
+
+    })
+    
+});
+
+// GET Buscar por nombre o telefono
+app.get( '/contacto/buscar/:termino', (req, res) => {
+    let termino = req.params.termino;
+    
+    /** Se crea una expresión regular basada en el término y 
+     * se le pasa como valor condisional */
+    let regex = new RegExp(termino, 'i');
+    Contacto.find({
+        $and:[{
+            $or:[
+                { nombre: regex }, 
+                { telefono: regex }
+            ]}, 
+            { activo:true }
+        ]},
+        'nombre apellido telefono email direccion' )
+        .sort('nombre')
+        .exec( (err, contactos) => {
+            if ( err ) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            
+            res.json({
+                ok: true,
+                contactos
+            })
+        });
+        
+});
+
+/**
+ * </GETS>
+ */
+
 
 
 
