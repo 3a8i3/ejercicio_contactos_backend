f31161f129222337ea637ecec29b3fc40e4331e4
3a8i3 <adavidruizdiaz@gmail.com>
2019-04-06 16:55:18 -0400

    primera subida

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+// Puerto
+process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * Configuración del entorno
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/**
+ * Configuración de la base de datos
+ */
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+	urlDB = 'mongodb://localhost:27017/taller3-contacto-db';
+	
+} else {
+
+	urlDB = process.env.DB_URI;
+	
+}
+
+process.env.URLDB = urlDB;
+
